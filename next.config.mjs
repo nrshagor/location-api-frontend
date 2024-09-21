1ba5db76798e0630483c3edfb15addc3ac3269a6
nrshagor <noorerabbishagor@gmail.com>
2024-09-21 13:01:27 +0600

    next config file setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,28 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const domain = process.env.NEXT_PUBLIC_URL
+  ? process.env.NEXT_PUBLIC_URL.replace(/^https?:\/\//, "")
+  : "";
+
 const nextConfig = {
   distDir: "_next",
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/**",
-      },
-    ],
+    ...(isProd
+      ? {
+          // Use domains in production
+          domains: [domain],
+        }
+      : {
+          // Use remotePatterns in development
+          remotePatterns: [
+            {
+              protocol: "http", // Assuming local dev is on http
+              hostname: "localhost",
+              port: "3000", // Your local port, e.g., 3000
+              pathname: "/**", // Match all paths in local development
+            },
+          ],
+        }),
   },
 };
 
