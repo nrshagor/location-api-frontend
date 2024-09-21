532b402dc822d25ef846415c52272e15304d2089
nrshagor <noorerabbishagor@gmail.com>
2024-09-21 13:39:33 +0600

    footer position fixed

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-6 ">
+    <footer className="bg-gray-800 text-white py-6 fixed w-full bottom-0">
       <div className="container mx-auto flex flex-col md:flex-row justify-center gap-20 items-center">
         <div className="mb-4 md:mb-0">
           <ul className="flex space-x-4">
